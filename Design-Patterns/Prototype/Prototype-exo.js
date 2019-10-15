7d0f15a064cf7e1e368649a1426b53eaabd747f6
Soreine <hello@soreine.dev>
2019-10-15 15:18:50 +0200

    Prototype 1.

--- Design-Patterns/Prototype/Prototype-exo.js
+++ Design-Patterns/Prototype/Prototype-exo.js
@@ -21,23 +21,40 @@ class CalendarEvent {
   }
 
   // Returns a deep clone of this event
-  // clone() {}
+  // clone() {
 
   // Return a clone of this event with a participant added
   // addParticipant(name) {}
 }
 
+// const event1 = new CalendarEvent(
+//   "Gardening preparation",
+//   { day: 1, month: 3 },
+//   ["Steve", "Hilary"]
+// );
+
+// const event2 = event1.clone();
+// event2.name = "Work";
+// event2.participants.push("Samantha");
+// event1.name == "Gardening preparation"
+
 // Creates an array of initial Events
 function initializeCalendar() {
-  const event1 = new CalendarEvent("Gardening", { day: 1, month: 3 }, [
+  const event1 = new CalendarEvent(
+    "Gardening preparation",
+    { day: 1, month: 3 },
+    ["Steve", "Hilary"]
+  );
+  const event2 = new CalendarEvent("Gardening work", { day: 1, month: 3 }, [
     "Steve",
-    "Hilary"
+    "Hilary",
+    "Samantha"
   ]);
-
-  // const event2 = ...
-
-  // const event3 = ...
-
+  const event3 = new CalendarEvent("Gardening work", { day: 2, month: 3 }, [
+    "Steve",
+    "Hilary",
+    "Samantha"
+  ]);
   return [event1, event2, event3];
 }
 
