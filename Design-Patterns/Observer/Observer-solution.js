783c40fe16996132755f371bfaecc3cb60760881
Soreine <hello@soreine.dev>
2019-10-17 15:06:02 +0200

    Observer: #1

---
+++ Design-Patterns/Observer/Observer-solution.js
@@ -0,0 +1,125 @@
+/**
+ * The big Macaron button.
+ */
+class MacaronButton {
+  constructor() {
+    this.button = document.getElementById("macaron");
+  }
+
+  /**
+   * Reacts to click
+   */
+  onClick(callback) {
+    this.button.addEventListener("click", callback);
+  }
+}
+
+/**
+ * Display for the money the player has
+ */
+class MoneyDisplay {
+  constructor() {
+    this.counter = document.getElementById("money-counter");
+  }
+
+  displayAmount(money) {
+    this.counter.innerText = money;
+  }
+}
+
+/**
+ * Controls the UI for an upgrade
+ */
+class UpgradeUI {
+  constructor(id) {
+    this.elements = UpgradeUI.findUpgradeElements(id);
+  }
+
+  /**
+   * @param id The ID of the upgrade
+   * @return { price, button, counter } The HTML Elements
+   */
+  static findUpgradeElements(id) {
+    const element = document.getElementById(id);
+    const price = element.getElementsByClassName("price")[0];
+    const button = element.getElementsByClassName("image-button")[0];
+    const counter = element.getElementsByClassName("counter")[0];
+
+    return {
+      price,
+      button,
+      counter
+    };
+  }
+
+  /**
+   * Display the price of the upgrade
+   */
+  displayPrice(price) {
+    this.elements.price.innerText = price;
+  }
+
+  /**
+   * Display the number of upgrades owned
+   */
+  displayCount(count) {
+    this.elements.counter.innerText = count;
+  }
+
+  /**
+   * Make the upgrade non clickable
+   */
+  disable() {
+    this.elements.button.setAttribute("disabled", "true");
+  }
+
+  /**
+   * Make the upgrade clickable
+   */
+  enable() {
+    this.elements.button.removeAttribute("disabled");
+  }
+
+  /**
+   * Reacts to clicks
+   */
+  onClick(callback) {
+    this.elements.button.addEventListener("click", callback);
+  }
+}
+
+// ------------------------------------
+// Votre code ci-dessous
+// ------------------------------------
+
+class GameState {
+  constructor() {
+    this.money = 0;
+  }
+
+  moneyTransaction(transaction) {
+    this.money = transaction(this.money);
+  }
+}
+
+// This is called when the page is loaded.
+function init() {
+  const macaronButton = new MacaronButton();
+  const moneyDisplay = new MoneyDisplay();
+
+  const gameState = new GameState();
+
+  macaronButton.onClick(() => {
+    // Ajouter 1 euro
+    gameState.moneyTransaction(amount => amount + 1);
+    // Mettre a jour l'afficheur
+    moneyDisplay.displayAmount(gameState.money);
+  });
+}
+
+// const mamieTransaction = amount => amount + 10;
+// const buyMamie = amount => amount - 100;
+
+// gameState.moneyTransaction(mamieTransaction);
+
+window.onload = init;
